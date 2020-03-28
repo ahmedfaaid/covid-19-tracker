00603881c60c83e34de61b18e3de690ecd3a2435
Ahmed <ahmedfaaid.coding@gmail.com>
2020-03-28 12:04:23 -0400

    Add spinner + styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,7 @@
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-    *,
-    *::before,
-    *::after {
-        margin: 0;
-        padding: 0;
-        box-sizing: inherit;
-    }
-
-    body {
-        box-sizing: border-box;
-        position: relative;
-        background: #f0f0f0;
-    }
-`
+import '../css/styles.css'
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <GlobalStyle />
-            <Component {...pageProps} />
-        </>
-    )
+    return <Component {...pageProps} />
 }
 
 export default MyApp
