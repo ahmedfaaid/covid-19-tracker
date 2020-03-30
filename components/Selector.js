82bf3f8257cb6fc6b7969907fad72600f239eabf
Ahmed <ahmedfaaid.coding@gmail.com>
2020-03-30 14:06:01 -0400

    Only country code working

--- components/Selector.js
+++ components/Selector.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import useStatistics from '../src/useStatistics'
 
 import Spinner from './Spinner'
@@ -6,20 +6,34 @@ import LocalCases from './LocalCases'
 
 export default function Selector({ url }) {
     const { statistics, isLoading, error } = useStatistics(url)
-    const [selectedCountry, setSelectedCountry] = useState('CA')
+    const [selectedCountryCode, setSelectedCountryCode] = useState('CA')
+    const [country, setCountry] = useState('Canada')
+    const [province, setProvince] = useState('Ontario')
 
     if (isLoading) return <Spinner />
     if (error) return <p>There was an error</p>
 
-    const locations = statistics.locations
+    const locations = statistics.locations.sort()
+
+    // console.log(selectedCountryCode, country, province)
 
     return (
         <section>
             <h2>COVID-19 statistics by country</h2>
-            <select>
+            <select
+                onChange={e => {
+                    setSelectedCountryCode(e.target.value)
+                }}
+            >
                 {locations.map(({ id, country, country_code, province }) => (
-                    <option key={id} value={country_code}>
-                        {province ? `${country} - ${province}` : `${country}`}
+                    <option
+                        key={id}
+                        selected={selectedCountryCode === country_code}
+                        value={country_code}
+                    >
+                        {province && province !== ''
+                            ? `${country} - ${province}`
+                            : `${country}`}
                     </option>
                 ))}
             </select>
