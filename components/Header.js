9ad95269aef0ee61abbb369e4657b3a3e6c4ff66
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-04-02 13:20:32 -0400

    Switch emoji icon to FA

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faVirus } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = styled.nav`
     width: 100%;
@@ -19,9 +21,13 @@ export default function Header() {
     return (
         <Navbar>
             <div>
-                <span style={{ fontSize: '2rem' }}>🦠</span>
+                <span style={{ fontSize: '2rem' }}>
+                    <FontAwesomeIcon icon={faVirus} />
+                </span>
                 <LogoHeading>COVID-19 Tracker</LogoHeading>
-                <span style={{ fontSize: '2rem' }}>🦠</span>
+                <span style={{ fontSize: '2rem' }}>
+                    <FontAwesomeIcon icon={faVirus} />
+                </span>
             </div>
         </Navbar>
     )
