9b9485f5009408a8856e26e0ef0df636c5eb1354
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-11-12 20:20:21 -0500

    add recovered data to charts

--- components/LineChart.js
+++ components/LineChart.js
@@ -22,24 +22,74 @@ const ChartWrapper = styled.div`
     }
 `
 
-const SwitchButton = styled.div`
-    background: #dec1ff;
-    color: #fff;
-    width: 200px;
-    margin: 10px auto;
-    padding: 15px;
-    border: 1px solid #fff;
-    border-radius: 0.5em;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    cursor: pointer;
+// const SwitchButton = styled.div`
+//     background: #dec1ff;
+//     color: #fff;
+//     width: 200px;
+//     margin: 10px auto;
+//     padding: 15px;
+//     border: 1px solid #fff;
+//     border-radius: 0.5em;
+//     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+//     cursor: pointer;
 
-    &:hover {
-        background: #7d70ba;
-        transition: all 0.1s ease-in;
+//     &:hover {
+//         background: #7d70ba;
+//         transition: all 0.1s ease-in;
+//     }
+
+//     &:active {
+//         transform: translateY(2px);
+//     }
+// `
+
+const Select = styled.select`
+    display: block;
+    font-size: 16px;
+    font-family: sans-serif;
+    font-weight: 700;
+    color: #444;
+    line-height: 1.3;
+    padding: 0.6em 1.4em 0.5em 0.8em;
+    width: 40%;
+    max-width: 100%;
+    box-sizing: border-box;
+    margin: auto;
+    border: 1px solid #aaa;
+    box-shadow: 0 1px 0 1px rgba(0, 0, 0, 0.04);
+    border-radius: 0.5em;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    background-color: #fff;
+    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+        linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%);
+    background-repeat: no-repeat, repeat;
+    background-position: right 0.7em top 50%, 0 0;
+    background-size: 0.65em auto, 100%;
+
+    &::-ms-expand {
+        display: none;
     }
 
-    &:active {
-        transform: translateY(2px);
+    &:hover {
+        border-color: #dec1ff;
+    }
+
+    &:focus {
+        border-color: #7d70ba;
+        box-shadow: 0 0 1px 3px rgba(59, 153, 252, 0.7);
+        box-shadow: 0 0 0 3px -moz-mac-focusring;
+        color: #222;
+        outline: none;
+    }
+
+    option {
+        font-weight: normal;
+    }
+
+    @media ${device.mobileL} {
+        min-width: 60%;
     }
 `
 
@@ -57,6 +107,7 @@ function LineChart({ countryName }) {
     const dates = statistics[countryName].map(data => data.date)
     const confirmedData = statistics[countryName].map(data => data.confirmed)
     const deathsData = statistics[countryName].map(data => data.deaths)
+    const recoveredData = statistics[countryName].map(data => data.recovered)
 
     // chart options
     const options = {
@@ -75,6 +126,8 @@ function LineChart({ countryName }) {
                 ? '#7d70ba'
                 : dataShown === 'Deaths'
                 ? '#424242'
+                : dataShown === 'Recovered'
+                ? '#87ceeb'
                 : null
         ],
         title: {
@@ -83,6 +136,8 @@ function LineChart({ countryName }) {
                     ? 'Confirmed cases'
                     : dataShown === 'Deaths'
                     ? 'Deaths'
+                    : dataShown === 'Recovered'
+                    ? 'Recoveries'
                     : null,
             style: {
                 fontSize: '16px',
@@ -102,30 +157,20 @@ function LineChart({ countryName }) {
                     ? confirmedData
                     : dataShown === 'Deaths'
                     ? deathsData
+                    : dataShown === 'Recovered'
+                    ? recoveredData
                     : null
         }
     ]
 
-    // toggle method
-    const switchData = () => {
-        if (dataShown === 'Confirmed') {
-            setDataShown('Deaths')
-        } else if (dataShown === 'Deaths') {
-            setDataShown('Confirmed')
-        }
-    }
-
     return (
         <ChartWrapper>
             <Chart options={options} series={series} type='line' />
-            <SwitchButton onClick={switchData}>
-                Switch to{' '}
-                {dataShown === 'Confirmed'
-                    ? 'Deaths'
-                    : dataShown === 'Deaths'
-                    ? 'Confirmed'
-                    : null}
-            </SwitchButton>
+            <Select onChange={e => setDataShown(e.target.value)}>
+                <option value='Confirmed'>Confirmed</option>
+                <option value='Deaths'>Deaths</option>
+                <option value='Recovered'>Recoveries</option>
+            </Select>
         </ChartWrapper>
     )
 }
